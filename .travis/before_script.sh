2f9dfff50274ac7619cd566d3fd1faa7effb3ba1
Gijs Molenaar <gijs@pythonic.nl>
2016-01-15 15:27:25 +0200

    use custom boost-python bottle

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -14,34 +14,15 @@ wget ftp://ftp.astron.nl/outgoing/Measures/WSRT_Measures.ztar
 tar zxvf WSRT_Measures.ztar
 
 
-# We don't want to build/test Python3 support for OSX since that requires a rebuild of boost-python.
-# This will take ages and will timeout the travis build.
-#
-if [ "$TRAVIS_OS_NAME" = osx ]; then
-    cmake .. \
-        -DUSE_FFTW3=ON \
-        -DBUILD_TESTING=ON \
-        -DUSE_OPENMP=OFF \
-        -DUSE_HDF5=ON \
-        -DBUILD_PYTHON=ON \
-        -DBUILD_PYTHON3=OFF \
-        -DDATA_DIR=$PWD \
-        -DSOFA_ROOT_DIR=$HOME \
-        -DCXX11=$CXX11 \
-        -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR}/installed
-else
-    cmake .. \
-        -DUSE_FFTW3=ON \
-        -DBUILD_TESTING=ON \
-        -DUSE_OPENMP=OFF \
-        -DUSE_HDF5=ON \
-        -DBUILD_PYTHON=ON \
-        -DBUILD_PYTHON3=ON \
-        -DDATA_DIR=$PWD \
-        -DSOFA_ROOT_DIR=$HOME \
-        -DCXX11=$CXX11 \
-        -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR}/installed
-fi
-
-
+cmake .. \
+    -DUSE_FFTW3=ON \
+    -DBUILD_TESTING=ON \
+    -DUSE_OPENMP=OFF \
+    -DUSE_HDF5=ON \
+    -DBUILD_PYTHON=ON \
+    -DBUILD_PYTHON3=ON \
+    -DDATA_DIR=$PWD \
+    -DSOFA_ROOT_DIR=$HOME \
+    -DCXX11=$CXX11 \
+    -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR}/installed
 
--- .travis/install.sh
+++ .travis/install.sh
@@ -6,5 +6,7 @@ set -x
 if [ "$TRAVIS_OS_NAME" = osx ]; then
     brew update >/dev/null
     brew tap homebrew/science
-    brew install cfitsio wcslib fftw hdf5 boost-python
+    brew install cfitsio wcslib fftw hdf5
+    curl -O https://bintray.com/artifact/download/casacore/homebrew-bottles/boost-python-1.60.0.el_capitan.bottle.1.tar.gz
+    brew install boost-python-1.60.0.el_capitan.bottle.1.tar.gz
 fi
